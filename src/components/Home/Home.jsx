c6b24ca2001ac5a7de763a34a229814dd9eb6fdb
Hari Narayanan <harinarayanan4057@gmail.com>
2023-10-18 22:09:32 +0530

    [Hari] Add solution for routing usecase with link tag

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './home.css';
 
 
@@ -6,8 +7,10 @@ import './home.css';
 export const Home = () =>{
     return <div className="home-container text-2xl">
         Welcome to the books catalog experiment! Click the below button to check the catalog!
+        <Link to="/books">
         <button className="mt-6 py-2 px-3 bg-indigo-500 text-white text-lg font-semibold rounded-md shadow-lg shadow-indigo-500/50 focus:outline-none">
             Go to catalog
         </button>
+        </Link>
     </div>
 }
